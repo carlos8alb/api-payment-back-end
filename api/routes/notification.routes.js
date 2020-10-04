1b2adecaefe1ea213d4124abd467c3186ba05df4
Carlos Albarracin <carlos8_alb@hotmail.com>
2020-10-04 11:38:18 +1100

    Add get all notification route

--- api/routes/notification.routes.js
+++ api/routes/notification.routes.js
@@ -4,7 +4,7 @@ module.exports = function({ NotificationController }) {
     const router = Router();
 
     router.post("/", NotificationController.createNotification.bind(NotificationController));
-    // router.get("/", NotificationController.getNotifications.bind(NotificationController));
+    router.get("/", NotificationController.getNotifications.bind(NotificationController));
     // router.get("/:id", NotificationController.getNotification.bind(NotificationController));
     // router.put("/:id", NotificationController.updateNotification.bind(NotificationController));
     // router.delete("/:id", NotificationController.deleteNotification.bind(NotificationController));
