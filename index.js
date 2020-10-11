246a9079018b5ce316480cd46f63eb477b7b8b19
Carlos Albarracin <carlos8_alb@hotmail.com>
2020-10-11 19:16:46 +1100

    Added logger info and error

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const container = require('./api/container');
 const application = container.resolve('app');
 const db = container.resolve('db');
 const config = container.resolve('config');
+const { loggerInfo, loggerError } = require('./utils');
 
 application
     .start()
     .then(async() => {
         // if (config.ENV === 'development') { await db.sequelize.sync({ force: false }) };
         await db.sequelize.sync({ force: false, alter: true });
-        console.log('Environment:', config.ENV);
+        loggerInfo.info(`Environment: ${ config.ENV }`);
         await db.sequelize.authenticate();
-        console.log('Connected to', config.DB.database, 'database');
+        loggerInfo.info(`Connected to ${ config.DB.database } database`);
     })
     .catch(err => {
-        console.log(err);
+        loggerError.error(JSON.stringify(err));
         process.exit();
     });
--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,19 @@
+const { createLogger, format, transports } = require('winston');
+
 function removeWhitespace(str) {
     return str.split(/\s+/).join(' ').trim();
 }
 
 function errorHandle(res, error) {
-
-    // console.log(error);
-
     let errorMessage = 'Ocurrió un error. Intente nuevamente';
     if (typeof error.errors !== 'undefined') {
         errorMessage = error.errors.map((error) => { return { 'field': error.path, 'message': error.message } })
     }
+
+    loggerError.error(`message: ${errorMessage}`);
+    loggerError.error(`error: ${error.message}`);
+    loggerError.error(`url: ${res.req.originalUrl}`);
+    loggerError.error(`params: ${JSON.stringify(res.req.params)}`);
 
     return res.status(400).json({
         success: false,
@@ -18,7 +22,47 @@ function errorHandle(res, error) {
     })
 }
 
+const loggerInfo = createLogger({
+    level: 'info',
+    format: format.combine(
+        format.simple(),
+        format.timestamp(),
+        format.printf(info => `[${ info.timestamp}] ${info.message }`)
+    ),
+    transports: [
+        new transports.File({
+            maxsize: 5120000,
+            minsize: 5,
+            filename: 'logs/logs.log',
+        }),
+        new transports.Console({
+            level: 'debug'
+        })
+    ]
+});
+
+const loggerError = createLogger({
+    level: 'error',
+    format: format.combine(
+        format.simple(),
+        format.timestamp(),
+        format.printf(info => `[${ info.timestamp}] ${info.message }`)
+    ),
+    transports: [
+        new transports.File({
+            maxsize: 5120000,
+            minsize: 5,
+            filename: 'logs/logs-error.log',
+        }),
+        new transports.Console({
+            level: 'debug'
+        })
+    ]
+});
+
 module.exports = {
     removeWhitespace,
-    errorHandle
+    errorHandle,
+    loggerInfo,
+    loggerError
 }
